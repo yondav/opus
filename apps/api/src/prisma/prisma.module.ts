8c3fb9a5e911ffe263e699e382e5ef578e91fe96
yondav <yoni@yondav.us>
2024-01-26 09:59:55 -0500

    docs(apps/api): adds comment to prisma module

--- apps/api/src/prisma/prisma.module.ts
+++ apps/api/src/prisma/prisma.module.ts
@@ -4,6 +4,9 @@ import { Module } from '@nestjs/common';
 
 import { PrismaService } from './prisma.service';
 
+/**
+ * PrismaModule is responsible for handling ORM integration.
+ */
 @Module({
   // Declare the PrismaService as a provider within the module
   providers: [PrismaService],
