088bfe5744c97ece7195bf8a046498ccbba8f926
yondav <yoni@yondav.us>
2024-01-31 09:30:15 -0500

    chore(apps/api): configures cors, session and logger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,10 @@
 // src/main.ts
 
+import { Logger } from '@nestjs/common';
+import type { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 import { AppModule } from './app.module';
 
@@ -13,9 +17,34 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   // Create an instance of the Nest application
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true, // Set to true to prevent access via JavaScript
+        secure: process.env.NODE_ENV === 'production', // Set to true in production if using HTTPS
+        maxAge: Number(process.env.SESSION_EXPIRY), // Set the max age of the cookie (optional)
+      },
+    })
+  );
+
+  app.use(passport.initialize());
+
+  const corsOptions: CorsOptions = {
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    exposedHeaders: ['refresh-token'],
+  };
+
+  app.enableCors(corsOptions);
 
   // Set a global prefix for all routes (e.g., /api)
   app.setGlobalPrefix('api');
+
+  app.useLogger(new Logger());
 
   // Start the application and listen on the specified port (from environment variables)
   await app.listen(process.env.PORT);
