91b3000fe4a3befe0e569da3be73c50631c6069a
yondav <yoni@yondav.us>
2024-01-31 02:28:06 -0500

    feat(apps/api): defines local login and signup methods in auth service

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -5,15 +5,18 @@ import { JwtService } from '@nestjs/jwt';
 import type { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { isEmpty } from 'class-validator';
+import type { Request } from 'express';
 
 import { UsersService } from '../users';
 import {
   ApiResponse,
+  BadRequestException,
   IsEmptyException,
   NotFoundException,
   UnauthorizedException,
   type DecodedJwtToken,
   type Nullable,
+  type UserWithToken,
 } from '../utils';
 
 import type * as Dto from './dtos';
@@ -130,6 +133,85 @@ export class AuthService {
     } catch (err) {
       // Return an error response in case of exceptions
       return ApiResponse.fromException(err);
+    }
+  }
+
+  /**
+   * Handles local user signup with email and password.
+   *
+   * @param {Dto.Signup} data - The signup data including email, password, and password confirmation.
+   * @returns {Promise<ApiResponse<UserWithToken>>} - ApiResponse containing user information with an access token or an error.
+   */
+  async localSignup({
+    email,
+    password,
+    passwordMatch,
+  }: Dto.Signup): Promise<ApiResponse<UserWithToken>> {
+    try {
+      // Check if registration data (email, password, passwordMatch) is empty
+      if (isEmpty({ email, password, passwordMatch }))
+        throw new IsEmptyException('registration data');
+
+      // Check if password and password confirmation match
+      if (password !== passwordMatch)
+        return ApiResponse.error(
+          new BadRequestException("password and confirmation password don't match")
+        );
+
+      // Create a new user with the provided email and password
+      const user = await this.createUser({ email, password });
+      // If user creation is unsuccessful, return an error response
+      if (!user.success) return ApiResponse.error(new BadRequestException(user.message));
+
+      // Log in the user and obtain an access token
+      const {
+        data: { access_token },
+      } = await this.login(user);
+
+      // Return a success response with the created user information and access token
+      return ApiResponse.success(
+        { ...user.data, access_token },
+        `user ${user.data.email} account successfully created ${
+          access_token ? 'and authenticated' : 'but not authenticated'
+        }`
+      );
+    } catch (err) {
+      // Return an error response in case of exceptions
+      return ApiResponse.error(err);
+    }
+  }
+
+  /**
+   * Logs in a user and generates an access token.
+   *
+   * @param {Request['user']} req - The user data from the request.
+   * @returns {Promise<ApiResponse<UserWithToken>>} - ApiResponse containing user information with an access token or an error.
+   */
+  async login(req: Request['user']): Promise<ApiResponse<UserWithToken>> {
+    try {
+      // Cast the user response from the request to ApiResponse<User>
+      const userResponse = req as ApiResponse<User>;
+
+      // Extract payload information from the user response
+      const payload = { email: userResponse.data.email, id: userResponse.data.id };
+
+      // Retrieve the user details from the database using the payload ID
+      const user = await this.usersService.getUserById(payload.id);
+
+      // Generate an access token using the payload information
+      const accessToken = this.generateJwtToken(payload);
+
+      // Return a success response with the user information and access token
+      return ApiResponse.success(
+        {
+          ...user.data,
+          access_token: accessToken,
+        },
+        `user ${payload.email} logged in`
+      );
+    } catch (err) {
+      // Return an error response in case of exceptions
+      return ApiResponse.error(err);
     }
   }
 }
