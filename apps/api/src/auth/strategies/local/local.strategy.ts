37e9f767f8c00da31a29efb72c41d5d46eee24c5
yondav <yoni@yondav.us>
2024-01-31 02:38:06 -0500

    feat(apps/api): handles serialization and deserialization in local strategy

--- apps/api/src/auth/strategies/local/local.strategy.ts
+++ apps/api/src/auth/strategies/local/local.strategy.ts
@@ -1,9 +1,13 @@
 // src/auth/strategies/local/local.strategy.ts
 
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import type { User } from '@prisma/client';
+import * as passport from 'passport';
 import { Strategy } from 'passport-local';
 
+import { UsersService } from '../../../users';
+import { UnauthorizedException } from '../../../utils';
 import { AuthService } from '../../auth.service';
 
 /**
@@ -12,15 +16,31 @@ import { AuthService } from '../../auth.service';
  */
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
-    super();
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService
+  ) {
+    super({ usernameField: 'email', passwordField: 'password', session: true });
+
+    // Serialize user (store user ID in the session)
+    passport.serializeUser((user: User, done) => {
+      done(null, user.id);
+    });
+
+    // Deserialize user (retrieve user from the session)
+    passport.deserializeUser(async (id: number, done) => {
+      const user = await this.usersService.getUserById(id); // Adjust this to your actual method
+      done(null, user.data.id);
+    });
   }
 
   async validate(username: string, password: string) {
     const user = await this.authService.validateUser(username, password);
+
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('unable to validate user');
     }
+
     return user;
   }
 }
