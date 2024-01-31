b3b1257b478800e57b1ee79c57522d38b22e5a48
yondav <yoni@yondav.us>
2024-01-31 02:19:36 -0500

    feat(apps/api): defines create user method in auth service

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,40 +1,60 @@
 // src/auth/auth.service.ts
 
 import { Injectable } from '@nestjs/common';
+import type { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { isEmpty } from 'class-validator';
 
-import { PrismaService } from '../prisma';
+import { UsersService } from '../users';
+import { ApiResponse, IsEmptyException, UnauthorizedException } from '../utils';
+
+import type * as Dto from './dtos';
 
 /**
  * AuthService handles user authentication and validation.
  */
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly usersService: UsersService) {}
 
   /**
-   * Validates a user's credentials.
-   * @param email - The user's email address
-   * @param password - The user's password
-   * @returns A user object if validation is successful, otherwise null
+   * Creates a new user with the provided email and password.
+   *
+   * @param {Omit<Dto.Signup, 'passwordMatch'>} data - The user registration data.
+   * @returns {Promise<ApiResponse<User>>} - ApiResponse containing user information or an error.
    */
-  async validateUser(email: string, password: string) {
-    // Retrieve user information from the database based on the provided email
-    const user = await this.prisma.user.findUnique({ where: { email } });
+  async createUser(data: Omit<Dto.Signup, 'passwordMatch'>): Promise<ApiResponse<User>> {
+    try {
+      // Check if registration data (email and password) is empty
+      if (isEmpty(data)) throw new IsEmptyException('registration data');
 
-    // Compare the provided password with the hashed password stored in the database if the user exists
-    const passwordComparison = user
-      ? await bcrypt.compare(password, user.password)
-      : false;
+      const { email, password } = data;
 
-    // If password validation is successful, return user information without the password
-    if (passwordComparison) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-      const { password, ...result } = user;
-      return result;
+      // Check if a user with the provided email already exists
+      const existingUser = await this.usersService.getUserByEmail(email);
+
+      // If a user with the email exists, return an error response
+      if (existingUser.success)
+        return ApiResponse.error(
+          new UnauthorizedException(
+            `account already exists for ${existingUser.data.email}`
+          )
+        );
+
+      // Hash the user's password before creating the user
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create the user with the hashed password
+      const user = await this.usersService.createUser({
+        email,
+        password: hashedPassword,
+      });
+
+      // Return a success response with the created user information
+      return user;
+    } catch (err) {
+      // Return an error response in case of exceptions
+      return ApiResponse.fromException(err);
     }
-
-    // If validation fails, return null
-    return null;
   }
 }
